3d9634a2f1bd6a84f80c6025a68608576a0ba87b
Wouter Vanelslander <wouter.vanelslander@wunderkraut.com>
2016-10-18 16:45:27 +0200

    feature/WKS-293: Pipelinetest initial commit

---
+++ test/Insightly/Item/PipelineTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CultuurNet\ProjectAanvraag\Insightly\Item;
+
+use CultuurNet\ProjectAanvraag\Insightly\AbstractInsightlyClientTest;
+use CultuurNet\ProjectAanvraag\JsonAssertionTrait;
+
+class PipelineTest extends AbstractInsightlyClientTest
+{
+    use JsonAssertionTrait;
+
+    /**
+     * Test Pipeline
+     */
+    public function testPipeline()
+    {
+
+    }
+}
