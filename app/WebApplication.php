d63c23fa8e356921838259fb04e15d6b27ec3dce
Wouter Vanelslander <wouter.vanelslander@wunderkraut.com>
2016-10-31 17:30:11 +0100

    feature/PROJ-6: Coding standards

--- app/WebApplication.php
+++ app/WebApplication.php
@@ -85,11 +85,13 @@ class WebApplication extends ApplicationBase
 
         $this['security.firewalls'] = [
             'unsecured' => [
-                'pattern' => MultiPathRequestMatcher::fromPaths([
-                    new Path('^/culturefeed/oauth', 'GET'),
-                    new Path('^/integration-types', 'GET'),
-                    new Path('^.*$', 'OPTIONS')
-                 ])
+                'pattern' => MultiPathRequestMatcher::fromPaths(
+                    [
+                        new Path('^/culturefeed/oauth', 'GET'),
+                        new Path('^/integration-types', 'GET'),
+                        new Path('^.*$', 'OPTIONS'),
+                    ]
+                ),
             ],
             'secured' => [
                 'pattern' => '^.*$',
