5205ad9d9c83189edb87196a18e624ae6e4f0555
Wouter Vanelslander <wouter.vanelslander@wunderkraut.com>
2016-10-31 11:12:05 +0100

    feature/PROJ-6: InsightlyClient remove method stubs

--- src/Insightly/InsightlyClient.php
+++ src/Insightly/InsightlyClient.php
@@ -146,49 +146,4 @@ class InsightlyClient implements InsightlyClientInterface
         $query = $this->addQueryFilters($options);
         return GetPipelinesResult::parseToResult($this->request(RequestInterface::GET, 'Pipelines', $query));
     }
-
-    public function getPipelineStages($options = [])
-    {
-        // TODO: Implement getPipelineStages() method.
-    }
-
-    public function getContacts($options = [])
-    {
-        // TODO: Implement getContacts() method.
-    }
-
-    public function getProductCategories($options = [])
-    {
-        // TODO: Implement getProductCategories() method.
-    }
-
-    public function getOrganisations($options = [])
-    {
-        // TODO: Implement getOrganisations() method.
-    }
-
-    public function updatePipeline(Pipeline $pipeline)
-    {
-        // TODO: Implement updatePipeline() method.
-    }
-
-    public function updateOrganisation(EntityInterface $organisation)
-    {
-        // TODO: Implement updateOrganisation() method.
-    }
-
-    public function insertProject(EntityInterface $project)
-    {
-        // TODO: Implement insertProject() method.
-    }
-
-    public function insertContact(EntityInterface $contact)
-    {
-        // TODO: Implement insertContact() method.
-    }
-
-    public function insertOrganisation(EntityInterface $organisation)
-    {
-        // TODO: Implement insertOrganisation() method.
-    }
 }
