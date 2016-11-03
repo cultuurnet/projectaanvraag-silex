e82b421c355d75e3ad0249b0335147596bdfef33
Wouter Vanelslander <wouter.vanelslander@wunderkraut.com>
2016-11-03 13:34:06 +0100

    feature/PROJ-16: Remove excess line break

--- app/Project/ProjectControllerProvider.php
+++ app/Project/ProjectControllerProvider.php
@@ -14,7 +14,6 @@ class ProjectControllerProvider implements ControllerProviderInterface
 {
     public function connect(Application $app)
     {
-
         $app['project_controller'] = function (Application $app) {
             return new ProjectController($app['command_bus'], $app['project_service'], $app['security.authorization_checker']);
         };
