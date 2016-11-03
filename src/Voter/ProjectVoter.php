32e298311b56305cbdd838f29055a5e49bf2a8c8
Wouter Vanelslander <wouter.vanelslander@wunderkraut.com>
2016-11-03 12:59:21 +0100

    feature/PROJ-16: Retain "activate" attribute in ProjectVoter

--- src/Voter/ProjectVoter.php
+++ src/Voter/ProjectVoter.php
@@ -12,6 +12,7 @@ class ProjectVoter extends Voter
 {
     const VIEW = 'view';
     const EDIT = 'edit';
+    const ACTIVATE = 'activate';
 
     /**
      * @param string $attribute
@@ -40,6 +41,6 @@ class ProjectVoter extends Voter
      */
     protected function supports($attribute, $object)
     {
-        return $object instanceof ProjectInterface && in_array($attribute, [self::VIEW, self::EDIT]);
+        return $object instanceof ProjectInterface && in_array($attribute, [self::VIEW, self::EDIT, self::ACTIVATE]);
     }
 }
