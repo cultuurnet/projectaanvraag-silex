7695f77282f20b31d1c8329faf409a2dab7ff63f
Thomas Jacobs <thomas.jacobs@wunderkraut.com>
2017-08-18 15:59:44 +0200

    Rename settings for template

--- src/Widget/WidgetType/SearchResults.php
+++ src/Widget/WidgetType/SearchResults.php
@@ -300,8 +300,8 @@ class SearchResults extends WidgetTypeBase
                 'result_count' => $result->getTotalItems(),
                 'events' => $this->formatEventData($result->getMember()->getItems(), 'nl'),
                 'pager' => $pager,
-                'settings' => $this->settings['items'],
-                'header' => $this->settings['header'],
+                'settings_items' => $this->settings['items'],
+                'settings_header' => $this->settings['header'],
             ]
         );
     }
