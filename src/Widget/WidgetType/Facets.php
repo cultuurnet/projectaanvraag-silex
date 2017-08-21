f4e49fcbb6f1de388091eeb155e89047b9371307
Thomas Jacobs <thomas.jacobs@wunderkraut.com>
2017-08-21 15:48:01 +0200

    Add sample facets results (so I can continue writing the rendering at least)

--- src/Widget/WidgetType/Facets.php
+++ src/Widget/WidgetType/Facets.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace CultuurNet\ProjectAanvraag\Widget\WidgetType;
+use CultuurNet\SearchV3\ValueObjects\FacetResult;
+use CultuurNet\SearchV3\ValueObjects\FacetResultItem;
+use CultuurNet\SearchV3\ValueObjects\FacetResults;
 
 /**
  * Provides the facets widget type.
@@ -36,6 +39,78 @@ class Facets extends WidgetTypeBase
      */
     public function render()
     {
+        // Sample facet results.
+        // @todo: These should be retrieved from the SearchResults query.
+        $facetResults = new FacetResults();
+        $facetResults->setFacetResults([
+            new FacetResult('themes', [
+                new FacetResultItem('1.40.0.0.0', [
+                    'nl' => 'Erfgoed',
+                ],2,[]),
+            ]),
+            new FacetResult('themes', [
+                new FacetResultItem('1.64.0.0.0', [
+                    'nl' => 'Milieu en natuur',
+                ],12,[]),
+            ]),
+            new FacetResult('themes', [
+                new FacetResultItem('1.51.14.0.0', [
+                    'nl' => 'Atletiek, wandelen en fietsen',
+                ],46,[]),
+            ]),
+            new FacetResult('themes', [
+                new FacetResultItem('1.37.2.0.0', [
+                    'nl' => 'Samenleving',
+                ],17,[]),
+            ]),
+            new FacetResult('types', [
+                new FacetResultItem('0.6.0.0.0', [
+                    'nl' => 'Beurs',
+                ],5,[]),
+            ]),
+            new FacetResult('types', [
+                new FacetResultItem('0.7.0.0.0', [
+                    'nl' => 'Begeleide uitstap of rondleiding',
+                ],11,[]),
+            ]),
+            new FacetResult('types', [
+                new FacetResultItem('0.3.1.0.0', [
+                    'nl' => 'Cursus of workshop',
+                ],27,[]),
+            ]),
+            new FacetResult('types', [
+                new FacetResultItem('0.3.1.0.0', [
+                    'nl' => 'Cursus of workshop',
+                ],27,[]),
+            ]),
+            new FacetResult('regions', [
+                new FacetResultItem('prv-vlaams-brabant', [
+                    'nl' => 'Vlaams-Brabant',
+                ],22,[]),
+            ]),
+            new FacetResult('regions', [
+                new FacetResultItem('prv-west-vlaanderen', [
+                    'nl' => 'West-Vlaanderen',
+                ],31,[]),
+            ]),
+            new FacetResult('regions', [
+                new FacetResultItem('prv-oost-vlaanderen', [
+                    'nl' => 'Oost-Vlaanderen',
+                ],29,[]),
+            ]),
+            new FacetResult('regions', [
+                new FacetResultItem('prv-antwerpen', [
+                    'nl' => 'Antwerpen',
+                ],62,[]),
+            ]),
+            new FacetResult('regions', [
+                new FacetResultItem('prv-limburg', [
+                    'nl' => 'Limburg',
+                ],17,[]),
+            ]),
+        ]);
+
+
         // Render twig with settings.
         return $this->twig->render(
             'widgets/facets-widget/facets-widget.html.twig',
