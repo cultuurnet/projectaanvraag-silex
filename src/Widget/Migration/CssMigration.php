6385bd2dcbda5b6a7670cf205348c9db63a98c21
Thomas Jacobs <thomas.jacobs@wunderkraut.com>
2017-09-13 12:42:12 +0200

    Create css migration class

---
+++ src/Widget/Migration/CssMigration.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CultuurNet\ProjectAanvraag\Widget\Migration;
+
+/**
+ * Class CssMigration
+ * @package CultuurNet\ProjectAanvraag\Widget\Migration
+ */
+class CssMigration
+{
+    public function __construct()
+    {
+        //
+    }
+
+}
