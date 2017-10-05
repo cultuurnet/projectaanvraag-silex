a59d2c44cc419ae22c887bcdd63b00177e7ddc2a
Thomas Jacobs <thomas.jacobs@wunderkraut.com>
2017-10-05 16:48:37 +0200

    Add js redirect function for extra filters

--- web/assets/js/widgets/facets/facets.js
+++ web/assets/js/widgets/facets/facets.js
@@ -46,4 +46,39 @@ window.CultuurnetWidgets = window.CultuurnetWidgets || { behaviors: {} };
         }
     };
 
+    /**
+     * Extra filter function.
+     *
+     * @param query
+     */
+    CultuurnetWidgets.extraFilter = function(query) {
+        var queryString = window.location.search;
+
+        if (queryString) {
+            // Convert existing query string to an object.
+            var currentParams = JSON.parse('{"' + decodeURI(queryString.substr(1).replace(/&/g, "\",\"").replace(/=/g, "\":\"")) + '"}');
+
+            // Split query value.
+            var split = query.split('=');
+
+            var param = split[0];
+            var value = split[1];
+
+            if (typeof currentParams[param] !== 'undefined' && value === 0) {
+                delete currentParams[param];
+            }
+            else {
+                currentParams[param] = value;
+            }
+
+            // Convert updated params to a query string.
+            var newParams = $.param(currentParams);
+
+            // TODO: Should this eventually work asynchronously?.
+            window.location.href = window.location.pathname + '?' + newParams;
+        } else {
+            window.location.href = window.location.pathname + '?' + query;
+        }
+    };
+
 })(CultuurnetWidgets, jQuery);
