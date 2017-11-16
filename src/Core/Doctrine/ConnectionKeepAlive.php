fe2426b38e7b9b1ecc1d0a7755cc59783ff03587
Nils Destoop <nils.destoop@gmail.com>
2017-11-16 16:48:40 +0100

    provide some comments

--- src/Core/Doctrine/ConnectionKeepAlive.php
+++ src/Core/Doctrine/ConnectionKeepAlive.php
@@ -6,7 +6,9 @@ use Doctrine\DBAL\Connection;
 
 declare(ticks=3000000);
 
-
+/**
+ * Provides a class to keep connections alive.
+ */
 class ConnectionKeepAlive
 {
     /**
@@ -15,18 +17,27 @@ class ConnectionKeepAlive
     protected $connections;
     protected $isAttached;
 
+    /**
+     * Construct the ConnectionKeepAlive.
+     */
     public function __construct()
     {
         $this->connections = [];
         $this->isAttached = false;
     }
 
+    /**
+     * Detach the keep alive.
+     */
     public function detach()
     {
         unregister_tick_function([$this, 'kick']);
         $this->isAttached = false;
     }
 
+    /**
+     * Attach the keep alive.
+     */
     public function attach()
     {
         if ($this->isAttached || register_tick_function([$this, 'kick'])) {
@@ -37,16 +48,31 @@ class ConnectionKeepAlive
         throw new \RuntimeException('Unable to attach keep alive to the system');
     }
 
+    /**
+     * Check if the connection exists.
+     * @param string $name
+     *
+     * @return bool
+     */
     public function hasConnection(string $name)
     {
         return isset($this->connections[$name]);
     }
 
+    /**
+     * Add a connection.
+     *
+     * @param string $name
+     * @param \Doctrine\DBAL\Connection $logConnection
+     */
     public function addConnection(string $name, Connection $logConnection)
     {
         $this->connections[$name] = $logConnection;
     }
 
+    /**
+     * Execute a query every tick to keep connection alive.
+     */
     public function kick()
     {
         foreach ($this->connections as $conn) {
