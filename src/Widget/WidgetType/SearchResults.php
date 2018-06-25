65bfdab6c1203c5134f81b3c54602759827210b9
Stijn Swaanen <swaanenstijn@gmail.com>
2018-06-25 12:28:02 +0200

    WID-132 facilities settings in widgetType type defenition

--- src/Widget/WidgetType/SearchResults.php
+++ src/Widget/WidgetType/SearchResults.php
@@ -91,6 +91,10 @@ use Symfony\Component\HttpFoundation\RequestStack;
  *                      "enabled":false,
  *                  }
  *              },
+ *              "facilities": {
+ *                  "enabled":false,
+ *                  "label": "Minstens 1 voorziening"
+ *              },
  *              "read_more":{
  *                  "enabled":true,
  *                  "label":"Lees verder"
