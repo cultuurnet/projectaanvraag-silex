505540a1679b0661f9e445b22a3ec0ce724f0ddd
brampauwelyn <brampauw@gmail.com>
2019-07-10 15:30:52 +0200

    WID-206 - fix code style errors

--- src/Widget/RegionService.php
+++ src/Widget/RegionService.php
@@ -69,12 +69,13 @@ class RegionService
      */
     public function sortByLevenshtein($matches, $searchString)
     {
-      usort($matches, function ($a, $b) use ($searchString) {
-          $levA = levenshtein($searchString, $a);
-          $levB = levenshtein($searchString, $b);
+        usort($matches, function ($a, $b) use ($searchString)
+        {
+            $levA = levenshtein($searchString, $a);
+            $levB = levenshtein($searchString, $b);
 
           return $levA === $levB ? 0 : ($levA > $levB ? 1 : -1);
-      });
+        });
 
       return $matches;
     }
