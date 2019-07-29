0788b6158f10d7fc6edc70353bc44da85e9261c0
brampauwelyn <brampauw@gmail.com>
2019-07-29 21:06:49 +0200

    WID-211 - add new Publishers type to allow array in Searchresults annotations

---
+++ src/Widget/Settings/Publishers.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CultuurNet\ProjectAanvraag\Widget\Settings;
+
+/**
+ * Provides a publishers type 
+ */
+class Publishers
+{
+
+    /**
+     * Cleanup the configuration.
+     */
+    public function cleanup($configuration)
+    {   
+        if(isset($configuration) && is_array($configuration)) {
+            return $configuration;
+        }
+        
+    }
+
+}
