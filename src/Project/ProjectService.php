dfdfc0a382031c59020dea9fb443b36340ad36fa
brampauwelyn <brampauw@gmail.com>
2019-08-02 11:09:58 +0200

    PROJ-111 - use getServiceConsumer instead of creating a new one when updating the contentfilter

--- src/Project/ProjectService.php
+++ src/Project/ProjectService.php
@@ -183,8 +183,8 @@ class ProjectService implements ProjectServiceInterface
         $project->setContentFilter($contentFilter);
 
         if ($project->getLiveConsumerKey()) {
-            $liveConsumer = new \CultureFeed_Consumer();
-            $liveConsumer->consumerKey = $project->getLiveConsumerKey();
+            $liveConsumerKey = $project->getLiveConsumerKey();
+            $liveConsumer = $this->culturefeedLive->getServiceConsumer($liveConsumerKey);
             if ($project->getSapiVersion() == '3') {
                 $liveConsumer->searchPrefixSapi3 = $project->getContentFilter();
             } else {
@@ -194,8 +194,8 @@ class ProjectService implements ProjectServiceInterface
         }
 
         if ($project->getTestConsumerKey()) {
-            $testConsumer = new \CultureFeed_Consumer();
-            $testConsumer->consumerKey = $project->getTestConsumerKey();
+            $testConsumerKey = $project->getTestConsumerKey();
+            $testConsumer = $this->culturefeedTest->getServiceConsumer($testConsumerKey);
             if ($project->getSapiVersion() == '3') {
                 $testConsumer->searchPrefixSapi3 = $project->getContentFilter();
             } else {
