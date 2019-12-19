5433c0f47ba411ce6b485d9b80c7561c7ea058e2
brampauwelyn <brampauw@gmail.com>
2019-12-19 09:11:01 +0100

    WID-260 - add extra check for typeId

--- src/Widget/Twig/TwigPreprocessor.php
+++ src/Widget/Twig/TwigPreprocessor.php
@@ -153,7 +153,7 @@ class TwigPreprocessor
             'vlieg' => $this->isVliegEvent($event),
             'uitpas' => $this->isUitpasEvent($event),
             'facilities' => $this->getFacilitiesWithPresentInformation($event),
-            'typeId' => $event->getTermsByDomain('eventtype')[0]->getId(),
+            'typeId' => ($event->getTermsByDomain('eventtype')[0]->getId()) ?: null,
         ];
 
         $defaultImage = $settings['image']['default_image'] ? $this->request->getScheme() . '://media.uitdatabank.be/static/uit-placeholder.png' : '';
