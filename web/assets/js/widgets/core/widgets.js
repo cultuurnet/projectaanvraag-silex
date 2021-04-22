902eb4b9d8e714b8693e5fde96795018aeaf0ada
Luc Wollants <lucwollants@gmail.com>
2021-04-22 14:08:58 +0200

    Encode the URL parameters to make sure brackets are encoded

--- web/assets/js/widgets/core/widgets.js
+++ web/assets/js/widgets/core/widgets.js
@@ -387,7 +387,7 @@ window.CultuurnetWidgets = window.CultuurnetWidgets || { behaviors: {} };
           newParams.push(key + '=' + value);
         });
 
-        return newParams.join('&');
+        return encodeURI(newParams.join('&'));
     };
 
     CultuurnetWidgets.addLoadEvent = function(func) {
