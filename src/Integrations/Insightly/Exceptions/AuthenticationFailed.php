ade2ca3fdd5bfc9360b15adea4771df04d0eda31
Luc Wollants <lucwollants@gmail.com>
2021-04-30 10:52:47 +0200

    Add exceptions for some common Insightly status codes
    These can be used inside the service/controller layer to take the correct actions

---
+++ src/Integrations/Insightly/Exceptions/AuthenticationFailed.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\ProjectAanvraag\Integrations\Insightly\Exceptions;
+
+use Exception;
+
+final class AuthenticationFailed extends Exception
+{
+
+}
