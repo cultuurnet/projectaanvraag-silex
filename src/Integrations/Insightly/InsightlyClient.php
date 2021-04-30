9c39d1c51e518df6542b363f059d9f0028bfbde8
Luc Wollants <lucwollants@gmail.com>
2021-04-30 10:54:35 +0200

    Add basic Insightly integration to manage a contact
    Managing a contact consists of adding, deleting or getting it. This is maybe more than the application requires but this allows to execute some integration tests and clean up test data.

---
+++ src/Integrations/Insightly/InsightlyClient.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\ProjectAanvraag\Integrations\Insightly;
+
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\Exceptions\AuthenticationFailed;
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\Exceptions\BadRequest;
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\Exceptions\DeleteFailed;
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\Exceptions\RecordLimitReached;
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\Exceptions\RecordNotFound;
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\Serializers\ContactSerializer;
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\Contact;
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\Id;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Psr7\Request;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+final class InsightlyClient
+{
+    /**
+     * @var ClientInterface
+     */
+    private $httpClient;
+
+    /**
+     * @var string
+     */
+    private $apiKey;
+
+    public function __construct(ClientInterface $httpClient, string $apiKey)
+    {
+        $this->httpClient = $httpClient;
+        $this->apiKey = $apiKey;
+    }
+
+    public function createContact(Contact $contact): Id
+    {
+        $request = new Request(
+            'POST',
+            'Contacts/',
+            $this->createHeaders(),
+            json_encode((new ContactSerializer())->toInsightlyArray($contact))
+        );
+
+        $response = $this->sendRequest($request);
+
+        $contactAsArray = json_decode($response->getBody()->getContents(), true);
+
+        return new Id($contactAsArray['CONTACT_ID']);
+    }
+
+    public function deleteContactById(Id $id): void
+    {
+        $request = new Request(
+            'DELETE',
+            'Contacts/' . $id->getValue(),
+            $this->createHeaders()
+        );
+
+        $this->sendRequest($request);
+    }
+
+    public function getContactById(Id $id): Contact
+    {
+        $request = new Request(
+            'GET',
+            'Contacts/' . $id->getValue(),
+            $this->createHeaders()
+        );
+
+        $response = $this->sendRequest($request);
+
+        $contactAsArray = json_decode($response->getBody()->getContents(), true);
+
+        return (new ContactSerializer())->fromInsightlyArray($contactAsArray);
+    }
+
+    private function createHeaders(): array
+    {
+        return [
+            'Authorization' => 'Basic ' . base64_encode($this->apiKey . ':'),
+            'Content-Type' => 'application/json',
+        ];
+    }
+
+    private function sendRequest(RequestInterface $request): ResponseInterface
+    {
+        $response = $this->httpClient->send($request);
+        $this->validateResponse($response);
+
+        return $response;
+    }
+
+    private function validateResponse(ResponseInterface $response): void
+    {
+        switch ($response->getStatusCode()) {
+            case 400:
+                throw new BadRequest($response->getReasonPhrase());
+            case 401:
+                throw new AuthenticationFailed($response->getReasonPhrase());
+            case 402:
+                throw new RecordLimitReached($response->getReasonPhrase());
+            case 404:
+                throw new RecordNotFound($response->getReasonPhrase());
+            case 417:
+                throw new DeleteFailed($response->getReasonPhrase());
+        }
+    }
+}
---
+++ test/Integrations/Insightly/InsightlyClientTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\ProjectAanvraag\Integrations\Insightly;
+
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\Exceptions\RecordNotFound;
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\Contact;
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\Email;
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\FirstName;
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\LastName;
+use GuzzleHttp\Client;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Yaml\Yaml;
+
+class InsightlyClientTest extends TestCase
+{
+    /**
+     * @var InsightlyClient
+     */
+    private $insightlyClient;
+
+    protected function setUp(): void
+    {
+        $config = Yaml::parse(file_get_contents(__DIR__ . '/../../../config.yml'));
+
+        $this->insightlyClient = new InsightlyClient(
+            new Client([
+                'base_uri' => $config['integrations']['insightly']['host'],
+                'http_errors' => false,
+            ]),
+            $config['integrations']['insightly']['api_key']
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_manage_contacts(): void
+    {
+        $expectedContact = new Contact(
+            new FirstName('Jane'),
+            new LastName('Doe'),
+            new Email('jane.doe@anonymous.com')
+        );
+
+        $id = $this->insightlyClient->createContact($expectedContact);
+
+        $actualContact = $this->insightlyClient->getContactById($id);
+        $this->assertEquals(
+            $expectedContact->withId($id),
+            $actualContact
+        );
+
+        $this->insightlyClient->deleteContactById($id);
+
+        $this->expectException(RecordNotFound::class);
+        $this->insightlyClient->getContactById($id);
+    }
+}
