2cc25619cd60057cf3b832d681e9b949e4c50c26
Luc Wollants <lucwollants@gmail.com>
2021-05-04 09:41:07 +0200

    Serialize and deserialize the custom field for the integration type

--- src/Integrations/Insightly/Serializers/OpportunitySerializer.php
+++ src/Integrations/Insightly/Serializers/OpportunitySerializer.php
@@ -7,12 +7,15 @@ namespace CultuurNet\ProjectAanvraag\Integrations\Insightly\Serializers;
 use CultuurNet\ProjectAanvraag\Integrations\Insightly\PipelineStages;
 use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\Description;
 use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\Id;
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\IntegrationType;
 use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\Name;
 use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\Opportunity;
 use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\OpportunityState;
 
 final class OpportunitySerializer
 {
+    private const CUSTOM_FIELD_INTEGRATION_TYPE = 'Product__c';
+
     /**
      * @var PipelineStages
      */
@@ -31,6 +34,13 @@ final class OpportunitySerializer
             'OPPORTUNITY_DETAILS' => $opportunity->getDescription()->getValue(),
             'PIPELINE_ID' => $this->pipelineStages->getOpportunitiesPipelineId(),
             'STAGE_ID' => $this->pipelineStages->getIdFromOpportunityStage($opportunity->getStage()),
+            'CUSTOMFIELDS' => [
+                [
+                    'FIELD_NAME' => self::CUSTOM_FIELD_INTEGRATION_TYPE,
+                    'CUSTOM_FIELD_ID' => self::CUSTOM_FIELD_INTEGRATION_TYPE,
+                    'FIELD_VALUE' => $opportunity->getIntegrationType()->getValue(),
+                ]
+            ]
         ];
 
         if ($opportunity->getId()) {
@@ -52,11 +62,20 @@ final class OpportunitySerializer
 
     public function fromInsightlyArray(array $insightlyArray): Opportunity
     {
+        $integrationType = null;
+        foreach ($insightlyArray['CUSTOMFIELDS'] as $customField) {
+            if ($customField['CUSTOM_FIELD_ID'] === self::CUSTOM_FIELD_INTEGRATION_TYPE) {
+                $integrationType = new IntegrationType($customField['FIELD_VALUE']);
+                break;
+            }
+        }
+
         return (new Opportunity(
             new Name($insightlyArray['OPPORTUNITY_NAME']),
             new OpportunityState($insightlyArray['OPPORTUNITY_STATE']),
             $this->pipelineStages->getOpportunityStageFromId($insightlyArray['STAGE_ID']),
-            new Description($insightlyArray['OPPORTUNITY_DETAILS'])
+            new Description($insightlyArray['OPPORTUNITY_DETAILS']),
+            $integrationType
         ))->withId(
             new Id($insightlyArray['OPPORTUNITY_ID'])
         );
