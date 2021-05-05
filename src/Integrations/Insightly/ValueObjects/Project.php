b35561571cfb0c2a00d28703a1b00086e6c0b782
Luc Wollants <lucwollants@gmail.com>
2021-05-05 10:03:08 +0200

    Add simple VO for Project

---
+++ src/Integrations/Insightly/ValueObjects/Project.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects;
+
+final class Project
+{
+    /**
+     * @var ?Id
+     */
+    private $id;
+
+    /**
+     * @var Name
+     */
+    private $name;
+
+    /**
+     * @var ProjectStage
+     */
+    private $stage;
+
+    /**
+     * @var ProjectStatus
+     */
+    private $status;
+
+    /**
+     * @var Description
+     */
+    private $description;
+
+    /**
+     * @var IntegrationType
+     */
+    private $integrationType;
+
+    /**
+     * @var Coupon
+     */
+    private $coupon;
+
+    public function __construct(
+        Name $name,
+        ProjectStage $stage,
+        ProjectStatus $status,
+        Description $description,
+        IntegrationType $integrationType,
+        Coupon $coupon
+    ) {
+        $this->name = $name;
+        $this->stage = $stage;
+        $this->status = $status;
+        $this->description = $description;
+        $this->integrationType = $integrationType;
+        $this->coupon = $coupon;
+    }
+
+    public function withId(Id $id): self
+    {
+        $clone = clone $this;
+        $clone->id = $id;
+        return $clone;
+    }
+
+    public function getId(): ?Id
+    {
+        return $this->id;
+    }
+
+    public function getName(): Name
+    {
+        return $this->name;
+    }
+
+    public function getStage(): ProjectStage
+    {
+        return $this->stage;
+    }
+
+    public function getStatus(): ProjectStatus
+    {
+        return $this->status;
+    }
+
+    public function getDescription(): Description
+    {
+        return $this->description;
+    }
+
+    public function getIntegrationType(): IntegrationType
+    {
+        return $this->integrationType;
+    }
+
+    public function getCoupon(): Coupon
+    {
+        return $this->coupon;
+    }
+}
