72459c0e76a05c75c58963b0f66624b374e4de07
Luc Wollants <lucwollants@gmail.com>
2021-05-06 16:10:26 +0200

    Add delete and get method to OrganizationResource

--- src/Integrations/Insightly/Resources/OrganizationResource.php
+++ src/Integrations/Insightly/Resources/OrganizationResource.php
@@ -42,4 +42,28 @@ final class OrganizationResource
         $organizationAsArray = json_decode($response->getBody()->getContents(), true);
         return new Id($organizationAsArray['ORGANISATION_ID']);
     }
+
+    public function deleteById(Id $id): void
+    {
+        $request = new Request(
+            'DELETE',
+            'Organizations/' . $id->getValue()
+        );
+
+        $this->insightlyClient->sendRequest($request);
+    }
+
+    public function getById(Id $id): Organization
+    {
+        $request = new Request(
+            'GET',
+            'Organizations/' . $id->getValue()
+        );
+
+        $response = $this->insightlyClient->sendRequest($request);
+
+        $organizationAsArray = json_decode($response->getBody()->getContents(), true);
+
+        return ($this->organizationSerializer->fromInsightlyArray($organizationAsArray));
+    }
 }
