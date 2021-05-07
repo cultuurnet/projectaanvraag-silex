6734ab3581ae5be3f41dfea36f11dd28acb45994
Luc Wollants <lucwollants@gmail.com>
2021-05-07 12:51:10 +0200

    Add the custom fields Tax and Email

--- src/Integrations/Insightly/Serializers/CustomFieldSerializer.php
+++ src/Integrations/Insightly/Serializers/CustomFieldSerializer.php
@@ -5,13 +5,17 @@ declare(strict_types=1);
 namespace CultuurNet\ProjectAanvraag\Integrations\Insightly\Serializers;
 
 use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\Coupon;
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\Email;
 use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\IntegrationType;
+use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\TaxNumber;
 use InvalidArgumentException;
 
 final class CustomFieldSerializer
 {
     private const CUSTOM_FIELD_INTEGRATION_TYPE = 'Product__c';
     private const CUSTOM_FIELD_COUPON = 'Coupon_field__c';
+    private const CUSTOM_FIELD_EMAIL = 'Email_boekhouding__c';
+    private const CUSTOM_FIELD_TAX_NUMBER = 'BTW_nummer__c';
 
     public function getIntegrationType(array $customFields): IntegrationType
     {
@@ -23,6 +27,16 @@ final class CustomFieldSerializer
         return new Coupon($this->getCustomFieldValue($customFields, self::CUSTOM_FIELD_COUPON));
     }
 
+    public function getEmail(array $customFields): Email
+    {
+        return new Email($this->getCustomFieldValue($customFields, self:: CUSTOM_FIELD_EMAIL));
+    }
+
+    public function getTaxNumber(array $customFields): TaxNumber
+    {
+        return new TaxNumber($this->getCustomFieldValue($customFields, self:: CUSTOM_FIELD_TAX_NUMBER));
+    }
+
     public function integrationTypeToCustomField(IntegrationType $integrationType): array
     {
         return $this->createCustomField(self::CUSTOM_FIELD_INTEGRATION_TYPE, $integrationType->getValue());
@@ -31,6 +45,16 @@ final class CustomFieldSerializer
     public function couponToCustomField(Coupon $coupon): array
     {
         return $this->createCustomField(self::CUSTOM_FIELD_COUPON, $coupon->getValue());
+    }
+
+    public function emailToCustomField(Email $email): array
+    {
+        return $this->createCustomField(self::CUSTOM_FIELD_EMAIL, $email->getValue());
+    }
+
+    public function taxNumberToCustomField(TaxNumber $taxNumber): array
+    {
+        return $this->createCustomField(self::CUSTOM_FIELD_TAX_NUMBER, $taxNumber->getValue());
     }
 
     private function getCustomFieldValue(array $customFields, string $key): string
