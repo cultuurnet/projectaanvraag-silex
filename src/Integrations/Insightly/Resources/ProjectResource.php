a9996b53697a56058b4119c88219bcf6fbec6c6e
Luc Wollants <lucwollants@gmail.com>
2021-05-19 16:13:07 +0200

    Add method create a project with a linked contact and a linked organization

--- src/Integrations/Insightly/Resources/ProjectResource.php
+++ src/Integrations/Insightly/Resources/ProjectResource.php
@@ -52,6 +52,15 @@ final class ProjectResource
         return $id;
     }
 
+    public function createWithContactAndOrganization(Project $project, Id $contactId, Id $organizationId): Id
+    {
+        $id = $this->createWithContact($project, $contactId);
+
+        $this->linkOrganization($id, $organizationId);
+
+        return $id;
+    }
+
     public function deleteById(Id $id): void
     {
         $request = new Request(
