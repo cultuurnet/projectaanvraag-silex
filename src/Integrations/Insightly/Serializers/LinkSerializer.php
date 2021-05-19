8a642a1b89a0d4b8f65e7579596b1c5c5a16e8f8
Luc Wollants <lucwollants@gmail.com>
2021-05-19 16:11:48 +0200

    Make it possible to also serialize an organization link
    Also converted the link object names to constants.

--- src/Integrations/Insightly/Serializers/LinkSerializer.php
+++ src/Integrations/Insightly/Serializers/LinkSerializer.php
@@ -8,26 +8,47 @@ use CultuurNet\ProjectAanvraag\Integrations\Insightly\ValueObjects\Id;
 
 final class LinkSerializer
 {
+    private const CONTACT_LINK_OBJECT_NAME = 'Contact';
+    private const ORGANIZATION_LINK_OBJECT_NAME = 'Organization';
+
     public function contactIdToLink(Id $contactId): array
     {
         return [
             'LINK_OBJECT_ID' => $contactId->getValue(),
-            'LINK_OBJECT_NAME' => 'Contact',
+            'LINK_OBJECT_NAME' => self::CONTACT_LINK_OBJECT_NAME,
             'ROLE' => 'Aanvrager',
+        ];
+    }
+
+    public function organizationIdToLink(Id $organizationId): array
+    {
+        return [
+            'LINK_OBJECT_ID' => $organizationId->getValue(),
+            'LINK_OBJECT_NAME' => self::ORGANIZATION_LINK_OBJECT_NAME,
         ];
     }
 
     public function contactIdFromLinks(array $links): Id
     {
-        $contactId = null;
+        return $this->getId($links, self::CONTACT_LINK_OBJECT_NAME);
+    }
+
+    public function organizationIdFromLinks(array $links): Id
+    {
+        return $this->getId($links, self::ORGANIZATION_LINK_OBJECT_NAME);
+    }
+
+    private function getId(array $links, string $linkObjectName): Id
+    {
+        $id = null;
 
         foreach ($links as $link) {
-            if ($link['LINK_OBJECT_NAME'] === 'Contact') {
-                $contactId = new Id($link['LINK_OBJECT_ID']);
+            if ($link['LINK_OBJECT_NAME'] === $linkObjectName) {
+                $id = new Id($link['LINK_OBJECT_ID']);
                 break;
             }
         }
 
-        return $contactId;
+        return $id;
     }
 }
