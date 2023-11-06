80b19763497a9a9ba51946bb2bbbb505f2c9bb73
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-11-06 13:15:11 +0100

    create trait to validate project request

---
+++ src/Project/Controller/ProjectControllerTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CultuurNet\ProjectAanvraag\Project\Controller;
+
+use CultuurNet\ProjectAanvraag\Core\Exception\MissingRequiredFieldsException;
+
+trait ProjectControllerTrait
+{
+    /**
+     * Validate if all required fields are in the data.
+     * @param \stdClass $data
+     * @throws MissingRequiredFieldsException
+     */
+    private function validateRequiredFields($requiredFields, \stdClass $data = null)
+    {
+        $emptyFields = [];
+        foreach ($requiredFields as $field) {
+            if (empty($data->$field)) {
+                $emptyFields[] = $field;
+            }
+        }
+
+        if (!empty($emptyFields)) {
+            throw new MissingRequiredFieldsException('Some required fields are missing: ' . implode(', ', $emptyFields));
+        }
+    }
+}
