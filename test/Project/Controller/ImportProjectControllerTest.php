43e5bb7707f5cb90ecbf8225e7c2613441023121
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-11-06 14:38:01 +0100

    use inline var

--- test/Project/Controller/ImportProjectControllerTest.php
+++ test/Project/Controller/ImportProjectControllerTest.php
@@ -26,11 +26,6 @@ class ImportProjectControllerTest extends TestCase
      */
     private $request;
 
-    /**
-     * @var \stdClass
-     */
-    private $formData;
-
     public function setUp()
     {
         $this->messageBus = $this->createMock(MessageBusSupportingMiddleware::class);
@@ -40,37 +35,34 @@ class ImportProjectControllerTest extends TestCase
         $this->controller = new ImportProjectController(
             $this->messageBus
         );
-
-        $this->formData = new \stdClass();
-        $this->formData->name = 'name';
-        $this->formData->summary = 'summary';
-        $this->formData->integrationType = 2;
-        $this->formData->termsAndConditions = 'termsAndConditions';
     }
 
     public function testImportProject()
     {
         $platformUuid = '158cb996-916e-4ee6-8534-e46683555e8c';
 
-        $formData = $this->formData;
+        $formData = new \stdClass();
+        $formData->name = 'name';
+        $formData->summary = 'summary';
+        $formData->integrationType = 2;
         $formData->userId = 'auth0|39f6bc3d-2ba9-4587-8602-4a00a2b6667d';
-        $formData->groupId = $formData->integrationType;
+        $formData->groupId = 2;
         $formData->testApiKeySapi3 = 'a77f461f-3837-49bc-b2a6-1a8f57bf30d6';
         $formData->liveApiKeySapi3 = 'de808573-cfc4-4990-b91b-cf5673b913ac';
 
         $this->request
             ->expects($this->any())
             ->method('getContent')
-            ->willReturn(json_encode($this->formData));
+            ->willReturn(json_encode($formData));
 
         $importProject = new ImportProject(
             $platformUuid,
-            $this->formData->userId,
-            $this->formData->name,
-            $this->formData->summary,
-            $this->formData->integrationType,
-            $this->formData->testApiKeySapi3,
-            $this->formData->liveApiKeySapi3
+            $formData->userId,
+            $formData->name,
+            $formData->summary,
+            $formData->groupId,
+            $formData->testApiKeySapi3,
+            $formData->liveApiKeySapi3
         );
         $this->messageBus
             ->expects($this->once())
