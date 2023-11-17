c5a9ba65717923746853fa0bc276af8e39119512
Luc Wollants <lucwollants@gmail.com>
2023-11-17 15:55:48 +0100

    Create `OpenProjectController` instead of `SessionController`

---
+++ src/Project/Controller/OpenProjectController.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\ProjectAanvraag\Project\Controller;
+
+use Guzzle\Http\Client;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+final class OpenProjectController
+{
+    /**
+     * @var Session
+     */
+    private $session;
+
+    /**
+     * @var string
+     */
+    private $platformUrl;
+
+    public function __construct(Session $session, string $platformUrl)
+    {
+        $this->session = $session;
+        $this->platformUrl = $platformUrl;
+    }
+
+    public function openProject(Request $request, string $id): RedirectResponse
+    {
+        $tokenString = $request->get('idToken');
+        $client = new Client();
+        $request = $client->get($this->platformUrl . $tokenString);
+        $response = $request->send();
+
+        if ($response->getStatusCode() !== 200) {
+            throw new \Exception('Invalid token');
+        }
+
+        $this->session->set('id_token', $tokenString);
+        return new RedirectResponse('http://localhost:4200/project/' . $id);
+    }
+}
