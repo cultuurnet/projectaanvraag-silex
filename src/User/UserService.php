80b45f53627d9a3544e000cb3c561effde042f3e
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-11-17 12:44:41 +0100

    implement cleint in UserService.php

--- src/User/UserService.php
+++ src/User/UserService.php
@@ -3,6 +3,7 @@
 namespace CultuurNet\ProjectAanvraag\User;
 
 use CultuurNet\UiTIDProvider\User\UserService as UiTIDUserService;
+use Guzzle\Http\Client;
 
 class UserService extends UiTIDUserService
 {
@@ -40,8 +41,12 @@ class UserService extends UiTIDUserService
                 $roles = ['uitid_user'];
                 $user->setRoles(array_merge($this->userRoleStorage->getRolesByUserId($user->id), $roles));
             } catch (\Exception $e) {
+                $client = new Client();
+                $request = $client->get('http://host.docker.internal:81/api/user', null, ['query' => ['idToken' => $id]]);
+                $response = $request->send();
+                $userFromPlatform = json_decode($response->getBody(true), true);
                 // Do a call to publiq-platform to get the user
-                $user = User::fromPlatformUser($id, 'Luc W');
+                $user = User::fromPlatformUser($id, $userFromPlatform['nickname']);
             }
 
             return $user;
