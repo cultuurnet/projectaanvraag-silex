47fd5944cf11f112632c10057c6b148a01851d41
Luc Wollants <lucwollants@gmail.com>
2023-11-17 15:56:28 +0100

    Load user from session id_token

--- src/User/UserService.php
+++ src/User/UserService.php
@@ -4,6 +4,7 @@ namespace CultuurNet\ProjectAanvraag\User;
 
 use CultuurNet\UiTIDProvider\User\UserService as UiTIDUserService;
 use Guzzle\Http\Client;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class UserService extends UiTIDUserService
 {
@@ -13,14 +14,18 @@ class UserService extends UiTIDUserService
     protected $userRoleStorage;
 
     /**
-     * @param \CultureFeed $cultureFeed
-     * @param UserRoleStorageInterface $userRoleStorage
+     * @var Session
      */
-    public function __construct(\CultureFeed $cultureFeed, UserRoleStorageInterface $userRoleStorage)
-    {
-        parent::__construct($cultureFeed);
+    private $session;
 
+    public function __construct(
+        \CultureFeed $cultureFeed,
+        UserRoleStorageInterface $userRoleStorage,
+        Session $session
+    ) {
+        parent::__construct($cultureFeed);
         $this->userRoleStorage = $userRoleStorage;
+        $this->session = $session;
     }
 
     /**
@@ -32,22 +37,28 @@ class UserService extends UiTIDUserService
         try {
             try {
                 $cfUser = $this->cultureFeed->getUser($id, self::INCLUDE_PRIVATE_FIELDS);
-                //var_dump($cfUser); exit;
-
                 // Cast to a User object that can be safely encoded to json and add the user roles.
                 $user = User::fromCultureFeedUser($cfUser);
 
-                // Add roles (always add uitid_user role)
-                $roles = ['uitid_user'];
-                $user->setRoles(array_merge($this->userRoleStorage->getRolesByUserId($user->id), $roles));
             } catch (\Exception $e) {
+                $idToken = $this->session->get('id_token');
+
                 $client = new Client();
-                $request = $client->get('http://host.docker.internal:81/api/user', null, ['query' => ['idToken' => $id]]);
+                $request = $client->get(
+                    'http://host.docker.internal:81/api/token/' . $idToken
+                );
                 $response = $request->send();
                 $userFromPlatform = json_decode($response->getBody(true), true);
-                // Do a call to publiq-platform to get the user
-                $user = User::fromPlatformUser($id, $userFromPlatform['nickname']);
+
+                $user = User::fromPlatformUser(
+                    $userFromPlatform['sub'],
+                    $userFromPlatform['nickname']
+                );
             }
+
+            // Add roles (always add uitid_user role)
+            $roles = ['uitid_user'];
+            $user->setRoles(array_merge($this->userRoleStorage->getRolesByUserId($user->id), $roles));
 
             return $user;
         } catch (\CultureFeed_ParseException $e) {
