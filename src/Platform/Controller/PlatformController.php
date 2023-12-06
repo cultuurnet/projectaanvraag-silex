2b51dffedf9b64c9f582f2834b5d900bc5fe863f
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-12-06 12:44:09 +0100

    Add PlatformController

---
+++ src/Platform/Controller/PlatformController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CultuurNet\ProjectAanvraag\Platform\Controller;
+
+use CultuurNet\UiTIDProvider\User\UserSessionService;
+use Guzzle\Http\Message\Response;
+use Symfony\Component\HttpFoundation\Request;
+class PlatformController
+{
+    /**
+     * @var UserSessionService
+     */
+    private $userSessionService;
+    public function __construct(UserSessionService $userSessionService)
+    {
+        $this->userSessionService = $userSessionService;
+    }
+
+    public function logout(Request $request): Response
+    {
+        $this->userSessionService->logout();
+        return new Response(204);
+    }
+}
