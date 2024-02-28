151db1df4bdeee617b4bc6921c7bf25b277f41ca
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2024-02-28 10:04:54 +0100

    WID-513 - Fix formatting

--- app/UitpasAPI/UitpasAPIServiceProvider.php
+++ app/UitpasAPI/UitpasAPIServiceProvider.php
@@ -10,33 +10,31 @@ use Pimple\Container;
 class UitpasAPIServiceProvider extends APIServiceProviderBase
 {
 
-        public function register(Container $pimple)
-        {
-            $pimple['uitpas_api'] = function (Container $pimple) {
-                return new UitpasClient(new Client($this->getConfig($pimple)));
-            };
+    public function register(Container $pimple)
+    {
+        $pimple['uitpas_api'] = function (Container $pimple) {
+            return new UitpasClient(new Client($this->getConfig($pimple)));
+        };
     
-            $pimple['uitpas_api_test'] = function (Container $pimple) {
-                $config = $this->getConfig($pimple);
-                $config['base_uri'] = $pimple['uitpas_api_test.base_url'];
-                $config['headers']['x-client-id'] = $pimple['uitpas_api_test.x_client_id'];
+        $pimple['uitpas_api_test'] = function (Container $pimple) {
+            $config = $this->getConfig($pimple);
+            $config['base_uri'] = $pimple['uitpas_api_test.base_url'];
+            $config['headers']['x-client-id'] = $pimple['uitpas_api_test.x_client_id'];
     
-                return new UitpasClient(new Client($config));
-            };
-        }
+            return new UitpasClient(new Client($config));
+        };
+    }
 
-        private function getConfig(Container $pimple)
-        {
-            return [
-                'base_uri' => $pimple['uitpas_api.base_url'],
-                'headers' => [
-                    'Content-type' => 'application/json; charset=utf-8',
-                    'Accept' => 'application/ld+json',
-                    'x-client-id' => $pimple['uitpas_api.x_client_id'],
-                ],
-                'handler' => $this->getHandlerStack('uitpas_api', $pimple),
-            ];
-        }
-
-    
+    private function getConfig(Container $pimple)
+    {
+        return [
+            'base_uri' => $pimple['uitpas_api.base_url'],
+            'headers' => [
+                'Content-type' => 'application/json; charset=utf-8',
+                'Accept' => 'application/ld+json',
+                'x-client-id' => $pimple['uitpas_api.x_client_id'],
+            ],
+            'handler' => $this->getHandlerStack('uitpas_api', $pimple),
+        ];
+    }
 }
