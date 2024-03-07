f3098a76ac1e33412e98d02f03bed3b9854d50a5
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2024-03-07 15:33:12 +0100

    Add SentryServiceProvider

---
+++ app/Sentry/SentryServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CultuurNet\ProjectAanvraag\Sentry;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Sentry\ErrorHandler;
+use function Sentry\init;
+
+final class SentryServiceProvider implements ServiceProviderInterface
+{
+
+
+    public function register(Container $app)
+    {
+        init([
+            'dsn' => $app['config']['sentry']['dsn'],
+            'environment' => $app['config']['sentry']['environment'],
+        ]);
+        ErrorHandler::registerOnceErrorHandler();
+    }
+}
