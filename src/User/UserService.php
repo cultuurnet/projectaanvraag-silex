c721b305047ec455d51e8a1d29c790088421a8da
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-12-05 09:24:06 +0100

    no longer check user on uitidv1

--- src/User/UserService.php
+++ src/User/UserService.php
@@ -4,6 +4,7 @@ namespace CultuurNet\ProjectAanvraag\User;
 
 use CultuurNet\ProjectAanvraag\Platform\PlatformClientInterface;
 use CultuurNet\UiTIDProvider\User\UserService as UiTIDUserService;
+use Exception;
 
 class UserService extends UiTIDUserService
 {
@@ -34,28 +35,22 @@ class UserService extends UiTIDUserService
     public function getUser($id)
     {
         try {
-            try {
-                $cfUser = $this->cultureFeed->getUser($id, self::INCLUDE_PRIVATE_FIELDS);
-                // Cast to a User object that can be safely encoded to json and add the user roles.
-                $user = User::fromCultureFeedUser($cfUser);
-            } catch (\Exception $e) {
-                $userFromPlatform = $this->platformClient->getCurrentUser();
-                if (empty($userFromPlatform)) {
-                    return null;
-                }
-
-                $user = User::fromPlatformUser(
-                    $userFromPlatform['sub'],
-                    $userFromPlatform['nickname']
-                );
+            $userFromPlatform = $this->platformClient->getCurrentUser();
+            if (empty($userFromPlatform)) {
+                return null;
             }
+
+            $user = User::fromPlatformUser(
+                $userFromPlatform['sub'],
+                $userFromPlatform['nickname']
+            );
 
             // Add roles (always add uitid_user role)
             $roles = ['uitid_user'];
             $user->setRoles(array_merge($this->userRoleStorage->getRolesByUserId($user->id), $roles));
 
             return $user;
-        } catch (\CultureFeed_ParseException $e) {
+        } catch (Exception $e) {
             return null;
         }
     }
